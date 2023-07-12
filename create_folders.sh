8985539ee28bb4b2bd28bc82413180525db2693b
Mario Arana <marioknaus22@gmail.com>
2023-07-12 03:11:02 +0200

    Improvement of the program flow

--- create_folders.sh
+++ create_folders.sh
@@ -1,35 +1,73 @@
 #!/bin/bash
 
+# ----------------------------------------------------------------------
+# Functions
+# ----------------------------------------------------------------------
+
+# Function to set the directory from user input
+# Input: none
+# Output: sets the "directory" variable
+set_directory() {
+    while true; do
+        echo "Directory: "
+        read -r input
+
+        if [[ -d "$input" ]]; then
+            directory="$input"
+            break
+        else
+            echo "The directory does not exist, try again."
+        fi
+    done
+}
+
 # Function to create a directory
+# Input: "directory" and "new_folder" names
+# Output: creates a new folder in the directory
 create_directory() {
-    local dir=$1
+    # use local to avoid overwriting global variables
+    local dir=$1 # $1 is the first argument passed to the function
     local new_folder=$2
+
     mkdir "$dir/${new_folder}"
 }
 
+create_folders() {
+    echo "Enter the name of the new folders you want to create (enter '-q' to exit):"
+
+    while true; do
+        echo "Name: "
+        read -r folder_name
+
+        if [[ "$folder_name" == "-q" ]]; then
+            echo "Exiting."
+            break
+        else
+            create_directory "$directory" "$folder_name"
+        fi
+    done
+
+    # list the directories
+    list_directories "$directory"
+}
+
 # Function to list the new directories
+# Input: "directory" name
+# Output: lists the new directories and their contents
 list_directories() {
     local dir=$1
     echo "Directories in ${dir}:"
-    ls "${dir}"/*/
+    ls "${dir}"/*/ # */ is a wildcard that matches all directories
 }
 
-# Ask for the path to the directory
+# ----------------------------------------------------------------------
+# Main script
+# ----------------------------------------------------------------------
+
+# Ask for the path to the directory and set it
 echo "Enter the path to the directory where you want to create the new folders:"
-read directory
+directory=""
+set_directory
 
-# Ask for folder names until "quit" is entered
-while true; do
-    echo "Enter a name for a new folder (or 'quit' to exit):"
-    read folder_name
-
-    if [[ "$folder_name" == "quit" ]]; then
-        echo "Exiting."
-        break
-    else
-        create_directory "$directory" "$folder_name"
-    fi
-done
-
-# list the directories
-list_directories "$directory"
+# Create the new folders
+create_folders
