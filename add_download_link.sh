b84326f09260186253a23dcbec8558b2922fe8df
Mario Arana <marioknaus22@gmail.com>
2023-11-30 23:09:25 +0100

    Initial version of the add_download_link.sh script

---
+++ add_download_link.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+# ====================================================================
+# Constants
+# ====================================================================
+
+DESCRIPTION="Description: This script is a tool for my personal use.\nUsage: add_download_link.sh <directory> <url>"
+
+# ====================================================================
+# Functions
+# ====================================================================
+
+# Check if the directory exists. If not, print the usage and exit.
+check_directory() {
+    if [ ! -d "$1" ]; then
+        printf "Error: Directory does not exist.\n"
+        printf "$DESCRIPTION\n"
+        exit 1
+    fi
+}
+
+check_url() {
+    if [[ ! $1 =~ ^https?:// ]]; then
+        printf "Error: Invalid URL.\n"
+        printf "$DESCRIPTION\n"
+        exit 1
+    fi
+}
+
+# ====================================================================
+# Main
+# ====================================================================
+#
+# Check if the number of arguments is correct. If not, print the usage and exit.
+if [ $# -ne 2 ]; then
+    printf "Error: Wrong number of arguments.\n"
+    printf "$DESCRIPTION\n"
+    exit 1
+fi
+
+directory=$1
+url=$2
+
+check_directory $directory
+check_url $url
+
+touch $directory/README.md
+echo "- [Download]($url)" >> $directory/README.md
+printf "Add download link to $directory/README.md - Done.\n"
+
+exit 0
+
